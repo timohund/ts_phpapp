edb2e526f0f066b94d876eda8ef3c85f429047f1
Timo Schmidt <timo-schmidt@gmx.net>
2015-10-10 15:31:17 +0200

    Set version to 0.0.5

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name 'ts_phpapp'
 description 'Cookbooks to develop a php based app on the lamp stack'
 maintainer 'Timo Schmidt'
 license ''
-version '0.0.4'
+version '0.0.5'
 
 depends 'ts_basebox', '= 0.1.4'
